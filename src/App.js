37d13b94420c75102027153e9e3b5b0d541d127b
SergioVed <vedankinsergij@gmail.com>
2023-11-10 18:15:35 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [color, setColor] = useState('red')
+  function changeColor() {
+    if (color === 'red') {
+      setColor('yellow')
+    } else if(color === 'yellow'){
+      setColor('green')
+    } else if(color === 'green'){
+      setColor('red')
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button onClick={changeColor} id='button'>Change color</button>
+      <div id='div' className={color}></div>
+    </>
   );
 }
 
